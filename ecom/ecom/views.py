18916fdcb0740e389e41e9facf930a9d583bdad0
bislara <biswajeetsahoo54@gmail.com>
2020-04-10 18:32:19 +0530

    form from forms.py and email validation

--- ecom/ecom/views.py
+++ ecom/ecom/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render,redirect
 
+from ecom.forms import ContactForm
+
 def home_page(request):
     # return HttpResponse ("HI WORLD !!")
     context = {
@@ -19,14 +21,18 @@ def about_page(request):
     return render(request,"home_page.htm",context)
 
 def contact_page(request):
-    # return HttpResponse ("HI WORLD !!")
+    contact_form = ContactForm(request.POST or None)
     context = {
         "title":"HELLO WORLD",
-        "content":"Welcome to the contact page"
+        "content":"Welcome to the contact page",
+        "form": contact_form
     }
-    if request.method == "POST":
-        print(request.POST)
-        print(request.POST.get("fullname"))
-        print(request.POST.get("email"))
-        print(request.POST.get("content"))
+
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method == "POST":
+    #     print(request.POST)
+    #     print(request.POST.get("fullname"))
+    #     print(request.POST.get("email"))
+    #     print(request.POST.get("content"))
     return render(request,"contact/view.htm",context)
