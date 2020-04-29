bd3fe66d6ed506a51639605034529b8714b76505
bislara <biswajeetsahoo54@gmail.com>
2020-04-29 22:28:22 +0530

    adding timestamp and few other in details page

--- ecom/products/models.py
+++ ecom/products/models.py
@@ -63,6 +63,7 @@ class Product(models.Model):
     price           = models.DecimalField(decimal_places=2, max_digits=20, default=10.99)
     image           = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
     featured        = models.BooleanField(default = False)
+    timestamp       = models.DateTimeField(auto_now_add=True)
 
     objects = ProductManager()
 
