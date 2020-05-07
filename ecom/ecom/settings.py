99241901a984236527bb354234d34d36cb0ce629
Biswajeet Sahoo <biswajeetsahoo54@gmail.com>
2020-05-07 23:54:49 +0530

    update for python anywhere

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR
 ]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn","static_root")
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn","static_root")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
+# MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
